337a3600e585c117e7f913d89abf39e602281358
songjy001 <songjy0001@163.com>
2019-09-22 20:32:57 +0800

    修改分支

--- server-consumer/src/main/java/cc/mrbird/consumer/controller/HelloController.java
+++ server-consumer/src/main/java/cc/mrbird/consumer/controller/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @GetMapping("/hello/{message}")
     public String hello(@PathVariable String message) {
-        return this.helloService.hello(message);
+        return this.helloService.hello(message+"!!");
     }
 }
